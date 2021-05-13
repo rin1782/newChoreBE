32fd44a54bc49eb38cf77f167698cdb7db08a9bf
rin1782 <colaerin@yahoo.com>
2021-05-13 03:45:50 -0400

    created controllers

--- app/controllers/api/v1/chores_controller.rb
+++ app/controllers/api/v1/chores_controller.rb
@@ -1,2 +1,50 @@
 class Api::V1::ChoresController < ApplicationController
+    before_action :find_chore, only: [:index, :show, :create, :update, :destroy]
+
+    def index
+        @chores = Chore.all 
+        render json: @chores
+    end
+
+    def show
+        render json: @chore
+    end
+
+    def create
+        @chore = Chore.new(chore_params)
+        if @chore.save
+            render json: @chore
+        else
+            render error: { error: "Unable to create."}, status: 400
+        end
+    end
+
+    def update
+        if @chore
+            @chore.update(chore_params)
+            render json: { message: "chore has been updated"}, status: 200
+        else
+            render json: { error: "Unable to update"}, status: 400
+        end
+    end
+
+    def destroy
+        if @chore
+            @chore.destroy
+            render json: { message: "Chore complete! Yay!"}, status: 200
+        else
+            render json: { error: "Unable to delete, try again!"}, status: 400
+        end
+    end
+
+    private
+
+    def chore_params
+        params.require(:chore).permit(:chore, :kid_id)
+    end
+
+    def find_chore
+        @chore = Chore.find(params[:id])
+    end
+
 end
--- app/controllers/api/v1/kids_controller.rb
+++ app/controllers/api/v1/kids_controller.rb
@@ -1,2 +1,40 @@
 class Api::V1::KidsController < ApplicationController
+    before_action :find_kid, only: [:index, :show, :create, :destroy]
+
+    def index
+        @kids = Kid.all 
+        render json: @kids
+    end
+
+    def show
+        render json: @kid
+    end
+
+    def create
+        @kid = Kid.new(kid_params)
+        if @kid.save
+            render json: @kid
+        else
+            render error: {error: "Unable to create."}, status: 400
+        end
+    end
+
+    def destroy 
+        if @kid
+            @kid.destroy
+            render json: {message: "Successfully removed from list."}, status: 200
+        else
+            render json: {error: "Unable to delete."}, status: 400
+    end
+
+    private
+
+    def kid_params
+        params.require(:kid).permit(:id, :name)
+    end
+
+    def find_kid
+        @kid = Kid.find(params[:id])
+    end
+
 end
